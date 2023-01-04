5a9d78194c00ca26b8bed59889a35da005e1752d
Your Name <grakina.production@gmail.com>
2023-01-03 18:16:19 -0800

    created new files secret.js, pool.js where we are able to access the database and added to git ignore

--- server/config/pool.js
+++ server/config/pool.js
@@ -1,13 +1,20 @@
 const path = require('path');
 const { Pool } = require('pg'); 
+const { DB_PASSWORD } = require(path.resolve(__dirname, '../../secrets.js')); 
 // const { DB_PASSWORD } = require(path.resolve(__dirname, '../../secrets.js'));
 
-const PG_URI = `postgres://pvalafej:OQWLyL6isnOEO10-9Q4wCEPhHeMYF6-K@berry.db.elephantsql.com/pvalafej`;
+// const PG_URI = `postgres://pvalafej:OQWLyL6isnOEO10-9Q4wCEPhHeMYF6-K@berry.db.elephantsql.com/pvalafej`;
 
-//postgres://tekdpxxk:${DB_PASSWORD}@mahmud.db.elephantsql.com/tekdpxxk
+const PG_URI = `postgres://pvalafej:${DB_PASSWORD}@berry.db.elephantsql.com/pvalafej`;
 
 const pool = new Pool({
     connectionString: PG_URI,
-  });
-  
+});
 
+function query(text, params) {
+    return pool.query(text, params); 
+}
+
+module.exports = {
+    query, 
+}
